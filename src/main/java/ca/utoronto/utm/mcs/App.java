b3bbdd6c210928808187e0533fb266d20b2f47a6
Samyakk123 <samyak.mt@gmail.com>
2020-09-28 20:14:54 -0400

    Finished AddActor and figured out implementation.

--- src/main/java/ca/utoronto/utm/mcs/App.java
+++ src/main/java/ca/utoronto/utm/mcs/App.java
@@ -10,6 +10,7 @@ public class App
     public static void main(String[] args) throws IOException
     {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
+        server.createContext("/api/addActor", new AddActor());
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
     }
